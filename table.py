9fd562cf4f76fb641adaee5f003d4f3fa362ff4f
Fae <>
2022-02-01 12:48:56 -0800

    added nice printing, and created defs

--- table.py
+++ table.py
@@ -3,14 +3,20 @@ import sys
 print("this program prints out a relative frequency of characters in a string")
 
 word = input()
+def getFrequency(word):
+    print("given input: " + word)
 
-print("given input: " + word)
+    charDict = {}
+    for char in word:
+        if char not in charDict:
+            charDict[char] = 1
+        else:
+            charDict[char] += 1
+    return charDict
+# for printing pretty, accepts a relative frequency data type
+def print_row(relativeFreqency):
+    for key in relativeFreqency:
+        print(key ,': ',relativeFreqency[key])
 
-charDict = {}
-for char in word:
-    if char not in charDict:
-        charDict[char] = 1
-    else:
-        charDict[char] += 1
-print(charDict)
-
+    
+print_row(getFrequency(word))
